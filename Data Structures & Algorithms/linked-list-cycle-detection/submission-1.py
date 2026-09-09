dbf40207f9ae0f8fdca1c7c105f306336e9ee8ef
jootkoo <127549211+jootkoo@users.noreply.github.com>
2026-09-09 17:04:25 -0400

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        #if never reaches null then has cycle
+        #idea is to add NODES to the hashset for reference
+        # OR use tortoise and hare method where there are 2 pointers
+        #1 slow 1 fast, eventually the fast will catch the slow if no end otherwise null
+        
+        #set sol o(n)
+        '''
+        hashset = set()
+        curr = head
+
+        while curr:
+            if curr in hashset:
+                return True
+            else:
+                hashset.add(curr)
+                curr = curr.next
+        return False
+        '''
+        #tortoise hare sol o(1)
+        slow, fast = head, head #both start at same pos
+
+        while fast and fast.next:
+            slow = slow.next #moves by 1
+            fast = fast.next.next #moves by 2
+            if fast == slow:
+                return True
+        return False
+
+
+
+
+
